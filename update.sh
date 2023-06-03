a5ef4b38ffcab9fe60ca49c298e3204c48fec54e
Austin Oaks <austin.oaks02@gmail.com>
2023-06-02 20:06:10 -0400

    add update.sh and i3bconfig

---
+++ update.sh
@@ -0,0 +1,6 @@
+ln zshrc ~/.zshrc
+ln zshrc.alias ~/.zshrc.alias
+ln kitty.conf ~/.config/kitty/kitty.conf
+ln i3config ~/.config/i3/config
+cp -rl scripts ~/.config/i3blocks/scripts
+ln git-prompt.sh ~/.gitprompt.sh
